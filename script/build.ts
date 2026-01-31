eb1aab286eba2b2fa5c7d404cea88ad8b6d991e7
siminel143-oss <siminel143@gmail.com>
2026-01-31 15:02:13 +0200

    Update build.ts

--- script/build.ts
+++ script/build.ts
@@ -36,7 +36,13 @@ async function buildAll() {
   await rm("dist", { recursive: true, force: true });
 
   console.log("building client...");
-  await viteBuild();
+await viteBuild({
+  root: "client",
+  build: {
+    outDir: "../dist/public",
+    emptyOutDir: false,
+  },
+});
 
   console.log("building server...");
   const pkg = JSON.parse(await readFile("package.json", "utf-8"));

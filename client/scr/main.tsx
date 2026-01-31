a6358e52cd367cb7ca36cf6ce0554fc4e3d2f817
siminel143-oss <siminel143@gmail.com>
2026-01-31 15:32:46 +0200

    Update main.tsx

--- client/scr/main.tsx
+++ client/scr/main.tsx
@@ -1,5 +1,12 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById("root")!).render(<App />);
+function App() {
+  return <div>It works</div>;
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
